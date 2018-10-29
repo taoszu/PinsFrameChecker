d910d2f5960584960421276c5136149603bac2aa
tao <645153817@qq.com>
2018-10-29 11:04:51 +0800

    :rocket: 初始化插件

--- app/src/p_shop/java/com/taoszu/codecheck/pins/shop/ShopActivity.kt
+++ app/src/p_shop/java/com/taoszu/codecheck/pins/shop/ShopActivity.kt
@@ -1,13 +1,13 @@
-package com.taoszu.codecheck.pins
+package com.taoszu.codecheck.pins.shop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.taoszu.codecheck.pins.shop.R
+import com.taoszu.codecheck.pins.R
 
-class MainActivity : AppCompatActivity() {
+class ShopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shop)
     }
 }
